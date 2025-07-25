4f57f8f068352bf3a185b8ef2df10703d8522a27
Abheesht Sharma <abheesht@google.com>
2025-07-25 14:48:05 +0530

    Debugging

--- examples/ml_perf_debug/dataloader.py
+++ examples/ml_perf_debug/dataloader.py
@@ -78,9 +78,7 @@ def create_dummy_dataset(
             to_yield_x = {
                 "dense_features": example["dense_features"],
                 "preprocessed_sparse_features": (
-                    sparse_feature_preprocessor.preprocess(
-                        example["sparse_features"], training=True
-                    )
+                    example["sparse_features"], training=True
                 ),
             }
             if "dense_lookups" in example:
