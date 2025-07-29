ea10554333f772a7d59e54bacbe4b1e932aa1b9b
Abheesht Sharma <abheesht@google.com>
2025-07-29 17:02:23 +0530

    Debug

--- keras_rs/src/layers/embedding/base_distributed_embedding.py
+++ keras_rs/src/layers/embedding/base_distributed_embedding.py
@@ -663,6 +663,7 @@ class DistributedEmbedding(keras.layers.Layer):
         placement_to_path_to_inputs = keras.tree.pack_sequence_as(
             self._placement_to_path_to_feature_config, flat_inputs
         )
+        import json
         print("--->cfg", json.dump(self._placement_to_path_to_feature_config, indent=4))
         print("---> ppi", placement_to_path_to_inputs)
 
