dac93c50e8ea2599e96f4f51a6e1dbb1287b7981
Abheesht Sharma <abheesht@google.com>
2025-07-29 09:16:36 +0530

    Debug

--- examples/ml_perf_debug/main.py
+++ examples/ml_perf_debug/main.py
@@ -144,7 +144,7 @@ def main(
 
     train_generator = generator(train_ds, training=True)
     for first_batch in train_generator:
-        model(first_batch[0])
+        print("--->", model(first_batch[0]))
         break
 
     # Train the model.
