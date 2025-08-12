5874e644a2ee0553d80912158ced99fdd2a625d5
Abheesht Sharma <abheesht@google.com>
2025-08-12 07:45:56 +0530

    Fixes

--- examples/ml_perf_debug/main.py
+++ examples/ml_perf_debug/main.py
@@ -171,8 +171,11 @@ def main(
 
     train_generator = generator(train_ds, training=True)
     for first_batch in train_generator:
-        print("--->", model(first_batch[0]))
+        print(first_batch)
         break
+    # for first_batch in train_generator:
+    #     print("--->", model(first_batch[0]))
+    #     break
 
     # Train the model.
     # model.fit(train_generator, epochs=1)
