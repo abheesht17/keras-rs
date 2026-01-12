f56af3823e93dacc51c5809775dc1c71df752f69
Abheesht Sharma <abheesht@google.com>
2026-01-12 20:06:09 +0530

    Convert to appropriate tensor type

--- examples/ml_perf/main.py
+++ examples/ml_perf/main.py
@@ -10,7 +10,6 @@ from functools import partial
 
 os.environ["KERAS_BACKEND"] = "jax"
 
-import numpy as np
 import jax
 import keras
 
@@ -35,6 +34,9 @@ keras.config.disable_traceback_filtering()
 
 
 def convert_to_jax_compatible(x):
+    import numpy as np
+    import jax.experimental.sparse as jax_sparse
+
     if isinstance(x, (jax.Array, jax_sparse.JAXSparse, np.ndarray)):
         print("---> jax.Array, JAXSparse, np.ndarray")
         return x
